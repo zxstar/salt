226082ddc4f3c6b6693b16619f8baa4157525b0c
Pedro Algarvio <pedro@algarvio.me>
2020-04-08 13:57:47 +0100

    Fix the remaining tornado imports

--- tests/unit/modules/test_random_org.py
+++ tests/unit/modules/test_random_org.py
@@ -3,20 +3,13 @@
     :codeauthor: Jayesh Kariya <jayeshk@saltstack.com>
 """
 
-# Import Python Libs
 from __future__ import absolute_import, print_function, unicode_literals
 
-# Import 3rd-party libs
 import pytest
-
-# Import Salt Libs
 import salt.modules.random_org as random_org
 from salt.ext.tornado.httpclient import HTTPClient
-
-# Import Salt Testing Libs
 from tests.support.mixins import LoaderModuleMockMixin
 from tests.support.unit import TestCase, skipIf
-from tornado.httpclient import HTTPClient
 
 
 def check_status():
